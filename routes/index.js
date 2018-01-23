30eb7d1f1d689cba253033c6a3af4d3da14af538
Alex Vallorosi <alexvallorosi@gmail.com>
2018-01-22 20:22:37 -0500

    header vid

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,4 @@
+const turbo = require('turbo360')({site_id: process.env.TURBO_APP_ID})
 const vertex = require('vertex360')({site_id: process.env.TURBO_APP_ID})
 const router = vertex.router()
 const controllers = require('../controllers')
